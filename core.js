ee65585f2265ea8b7574bc7e7e68e10ceb944c43
Sohail Nasir <jtdinc2000@gmail.com>
2017-04-28 23:22:17 +0100

    Create proper Tiling object

--- core.js
+++ core.js
@@ -1,34 +1,48 @@
-var Container = PIXI.Container, 
+
+var tileAddressArray = [
+    { tileName: 'Grass', x: 0, y: 32 },
+    { tileName: 'Grass', x: 0, y: 64 },
+    { tileName: 'Grass', x: 32, y: 128 },
+    { tileName: 'Grass', x: 128, y: 32 },
+    { tileName: 'Grass', x: 288, y: 256 }
+];
+
+
+var Container = PIXI.Container,
     autoDetectRenderer = PIXI.autoDetectRenderer,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
     TextureCache = PIXI.utils.TextureCache,
     Sprite = PIXI.Sprite;
-    Rectangle = PIXI.Rectangle;
+Rectangle = PIXI.Rectangle;
 
-    var stage = new Container(),
-    renderer = autoDetectRenderer(512,512);
-    document.body.appendChild(renderer.view);
+var stage = new Container(),
+    renderer = autoDetectRenderer(512, 512);
+document.body.appendChild(renderer.view);
 
-    loader.add("images/citytileset.png")
-            .on("progress", loadProgressHandler).load(setup);
+loader.add("images/citytileset.png")
+    .on("progress", loadProgressHandler).load(setup);
 
-function loadProgressHandler(loader, resource){
+function loadProgressHandler(loader, resource) {
     //TODO: Some logging here
 }
 
-function setup(){
+function setup() {
     var tileSet = TextureCache['images/citytileset.png'];
     var rectangle = new Rectangle(32, 0, 32, 32);
     tileSet.frame = rectangle;
 
-    for(i = 0; i < 512; i++){
-        var grassBlock = new Sprite(tileSet);
-    grassBlock.x = i * 32;
-    grassBlock.y = 32;
-    stage.addChild(grassBlock);
-    }
+    tileAddressArray.forEach(function (tileAddress) {
+        if (tileAddress.tileName === 'Grass') {
+            var grassBlock = new Sprite(tileSet);
+            grassBlock.x = tileAddress.x;
+            grassBlock.y = tileAddress.y;
+            stage.addChild(grassBlock);
+        }
+        //TODO: Allow other elements here
+    });
 
 
-    renderer.render(stage); 
+
+    renderer.render(stage);
 }
