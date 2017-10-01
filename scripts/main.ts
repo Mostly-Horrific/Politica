1deae5dc885b2df9c411772aee9f12daaeafad73
Sohail Nasir <jtdinc2000@gmail.com>
2017-10-01 17:29:23 +0100

    added some stuff to the canvas

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,14 +1,23 @@
 class Start{
     static screenCanvas: HTMLCanvasElement;
+    static titleImage: HTMLImageElement;
 
     public static main():number{
         this.screenCanvas = <HTMLCanvasElement>document.getElementById('screen');
         this.screenCanvas.style.backgroundColor = "#f0c108";
-        console.log("Welcome To Politca");
+        this.screenCanvas.getContext('2d').font = '28px sematarymedium';
+        this.screenCanvas.getContext('2d').fillText('POLITICA', 290, 50);
+        this.titleImage = new Image();
+        this.titleImage.src = "../images/globetitle.jpg";
+        
+        this.screenCanvas.getContext('2d').drawImage(this.titleImage, 33, 71, 600, 400, 21, 20, 287, 104);
         return 0;
     }
 }
 
 window.onload = ()=> {
     Start.main();
+    var fontHack = document.getElementById('font-hack');
+    fontHack.style.display = 'none'
+
 }
