82561022a04b75cb16c0aa79bf5fde39b4edc4dd
Sohail Nasir <jtdinc2000@gmail.com>
2017-10-27 00:18:21 +0100

    Fleshed out Candiate model

--- scripts/election.ts
+++ scripts/election.ts
@@ -1,3 +1,39 @@
+class Candidate{
+    public Name:string;
+    public PoliticalParty:string;
+    public DateOfBirth:Date;
+    public Bio:string;
+    public PoliticalCompassValues:PolitcalCompass
+    public PastResults: Array<PolitcalResult>;
+    public PoliticalPositions:Array<PoliticalPositions>;
+}
+
+class PolitcalCompass{
+    public Authoritarian:number;
+    public Libetarian:number;
+    public Left:number;
+    public right:number;
+}
+
+class PoliticalPositions{
+    public Issue: string
+    public Sector: string
+    public LeftValue:number;
+    public RightValue:number;
+}
+
+class PolitcalResult{
+    public Year:number;
+    public Month:number;
+    public ByElection:boolean;
+    public SwingPercentage: number;
+    public Votes:number;
+    public PoliticalParty:string;
+    public WasIncumbant:boolean;
+    public WasVictory:boolean;
+}
+
+
 export function calculateSwing(){
     return 300;
 }
