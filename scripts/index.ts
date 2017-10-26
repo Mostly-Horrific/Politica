f6f1237979417f8708b5c85c3629be820fa1ec99
Sohail Nasir <jtdinc2000@gmail.com>
2017-10-27 00:01:32 +0100

    Restructured Javascript game startup

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,30 +1,57 @@
 class GameEngine{
-    static screenCanvas: HTMLCanvasElement;
-    static titleImage: HTMLImageElement;
-    static screenCanvasContext: CanvasRenderingContext2D;
+    public screenCanvas: HTMLCanvasElement;
+    public screenCanvasContext: CanvasRenderingContext2D;
+    public gameState: GameState;
 
-    public static main():number{
+    constructor() {
         this.screenCanvas = <HTMLCanvasElement>document.getElementById('screen');
         this.screenCanvas.style.backgroundColor = "#000000";
         this.screenCanvasContext = this.screenCanvas.getContext('2d');
+    }    
+}
 
-        this.titleImage = new Image();
-        this.titleImage.onload = ()=> {
-            this.screenCanvasContext.drawImage(this.titleImage, 0, 25, 800, 680);
-            this.screenCanvasContext.font = '28px sematarymedium';
-            this.screenCanvasContext.fillStyle = 'blue';
-            this.screenCanvasContext.fillText('POLITICA', 290, 50);    
-            
-        }
-        this.titleImage.src = "images/globetitle.jpg";
+enum GameState {
+    Title,
+    IntroScreen
+}
 
-        return 0;
+function SetUpTitle(gameObject: GameEngine){
+    var titleImage: HTMLImageElement;
+    this.titleImage = new Image();
+    this.titleImage.src = "images/globetitle.jpg";
+    
+    this.titleImage.onload = ()=> {
+        gameObject.screenCanvasContext.drawImage(this.titleImage, 0, 25, 800, 680);
+        gameObject.screenCanvasContext.font = '28px sematarymedium';
+        gameObject.screenCanvasContext.fillStyle = 'yellow';
+        gameObject.screenCanvasContext.fillText('POLITICA', 290, 50);    
+        gameObject.screenCanvasContext.fillText('Press Space to start', 242, 350);    
+    }
+    gameObject.gameState = GameState.Title;
+}
+
+function InitStartGameScreen(gameObject: GameEngine){
+    gameObject.screenCanvasContext.clearRect(0, 0, gameObject.screenCanvas.width, gameObject.screenCanvas.height);
+    alert('If I could be bothered to do anything, this would be awesome indeed');
+    gameObject.gameState = GameState.IntroScreen;
+    alert(gameObject.gameState);
+}
+
+function keyBoardInput(event: KeyboardEvent, gameEngineObject: GameEngine){
+    if(event.keyCode == 32){
+        InitStartGameScreen(gameEngineObject);
+    }
+    if(event.keyCode == 8){
+        SetUpTitle(gameEngineObject);
     }
 }
 
 window.onload = ()=> {
-    GameEngine.main();
+    var gameEngineObject = new GameEngine();
     var fontHack = document.getElementById('font-hack');
+    document.addEventListener('keydown', (e)=> {keyBoardInput(e, gameEngineObject)});
+    SetUpTitle(gameEngineObject);
     fontHack.style.display = 'none'
 
 }
+
